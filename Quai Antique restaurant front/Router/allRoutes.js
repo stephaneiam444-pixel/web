fe6c0bb2801a601c18104a2521b5ae7feeae177a
cannibale <stephaneiam444@gmail.com>
2025-12-09 15:16:46 +0100

    creations carte

--- Quai Antique restaurant front/Router/allRoutes.js
+++ Quai Antique restaurant front/Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("accueil", "Accueil", "pages/home.html"),
     new Route("galerie", "La galerie", "pages/galerie.html"),
+    new Route("carte", "La carte", "pages/La carte.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename

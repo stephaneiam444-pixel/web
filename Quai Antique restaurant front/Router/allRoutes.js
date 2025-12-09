468a632ce8d7146623eede34270eaf92add731a4
cannibale <stephaneiam444@gmail.com>
2025-12-09 14:31:55 +0100

    ajout de la page galerie

--- Quai Antique restaurant front/Router/allRoutes.js
+++ Quai Antique restaurant front/Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 // Définir ici vos routes
 export const allRoutes = [
     new Route("accueil", "Accueil", "pages/home.html"),
+    new Route("galerie", "La galerie", "pages/galerie.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename

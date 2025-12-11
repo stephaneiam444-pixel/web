f5278956d6ddc190f0347a8d355ee728d75df26b
cannibale <stephaneiam444@gmail.com>
2025-12-11 16:49:24 +0100

    creation dossier auth +editpassword+account

--- Quai Antique restaurant front/Router/allRoutes.js
+++ Quai Antique restaurant front/Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("accueil", "Accueil", "pages/home.html"),
     new Route("galerie", "La galerie", "pages/galerie.html"),
     new Route("carte", "La carte", "pages/La carte.html"),
-    new Route("signin", "Connexion", "pages/signin.html"),
-    new Route("signup", "Inscription", "pages/signup.html"),
+    new Route("signin", "Connexion", "pages/auth/signin.html"),
+    new Route("signup", "Inscription", "pages/auth/signup.html"),
+    new Route("account", "Mon compte", "pages/auth/account.html"),
+    new Route("editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
